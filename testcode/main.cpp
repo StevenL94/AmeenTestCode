476c08191afea4192c90dae3ae426eaafb5e0592
Steven Lee <sl3881@nyu.edu>
2022-02-17 12:20:44 -0500

    Initial Commit

---
+++ testcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testcode
+//
+//  Created by Steven Lee on 2/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
